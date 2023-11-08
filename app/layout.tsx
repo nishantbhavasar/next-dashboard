f9ed1152865af469e7e4b82506c280b4679e46fe
nishant.bhavsar <nishant.bhavsar@esparkbizmail.com>
2023-11-08 10:17:56 +0530

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import { inter } from "./ui/fonts";
+import "./ui/global.css";
+import cx from "classnames";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let bodyClasses = cx(inter.className, `antialiased`);
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={bodyClasses}>{children}</body>
     </html>
   );
 }
