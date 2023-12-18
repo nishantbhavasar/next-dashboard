9741c1f990948cb6e97e52541bc3ed7a75b0f146
nishant.bhavsar <nishant.bhavsar@esparkbizmail.com>
2023-12-18 19:02:09 +0530

    chapter 13 complate chapter 14 WIP

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -7,38 +7,68 @@ import { z } from 'zod';
  
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number()
+  .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'],{
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 // INSEART INVOICE SERVER ACTION
-export async function createInvoice(formData:FormData){
-  try {
-    const rawFormData = Object.fromEntries(formData.entries()); // extract the from data from Action
-    const {customerId,status,amount} = CreateInvoice.parse(rawFormData); // validate form data
+export async function createInvoice(preState:State,formData:FormData){
+  // const rawFormData = Object.fromEntries(formData.entries()); // extract the from data from Action
+  const validatedFields = CreateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }else{  
+    const { customerId, amount, status } = validatedFields.data;
     const USD_TO_CENTS = 100;
     const amountInCents = amount * USD_TO_CENTS;//convert usd to cents
     const date = new Date().toISOString().split('T')[0]; // EX: '2023-12-14'
+    
+    // Insert data into the database
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // If a database error occurs, return a more specific error.
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
 
-    // insert Data in DB
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `
-
-  } catch (error) {
-
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
+    // Revalidate the cache for the invoices page and redirect the user.
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
   }
-  revalidatePath('/dashboard/invoices');// refresh page remove cache
-  redirect('/dashboard/invoices');
 }
 
 // UPDATE INVOICE SERVER ACTION
